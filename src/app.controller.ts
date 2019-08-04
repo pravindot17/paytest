ece5f24ce8d31f2d0b8cc82410b14819ad93de4c
Pravin Lolage <pravindot17@gmail.com>
2019-08-04 19:40:13 +0530

    added health service endpoint

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,17 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Res } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
+
+  @Get('health')
+  checkHealth(@Res() res): object {
+    return res.json({ status: 'working' });
+  }
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getTitle(@Res() res): string {
+    return res.send(this.appService.getTitle());
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getTitle(): string {
+    return '<h1>Payment test app!</h1>';
   }
 }
