53694268c48efaed15c38211487651e148b99cfc
Pravin <pravin.lolage@b2x.com>
2019-08-08 18:35:04 +0530

    removed class declaration bug

--- src/payment/dto/make-payment.dto.ts
+++ src/payment/dto/make-payment.dto.ts
@@ -1,18 +1,6 @@
 import { IsString, Length, ValidateNested, IsNumber } from 'class-validator';
 import { Type } from 'class-transformer';
 
-export class MakePaymentDto {
-  @IsString() @Length(10, 40)
-  public orderId: string;
-
-  @IsNumber()
-  public readonly totalAmount: number;
-
-  @ValidateNested({ each: true })
-  @Type(() => PaymentInfoDto)
-  public readonly paymentDetails: PaymentInfoDto;
-}
-
 class PaymentInfoDto {
   @IsString() @Length(3, 64)
   public readonly method: string;
@@ -25,4 +13,16 @@ class PaymentInfoDto {
 
   @IsString() @Length(4)
   public readonly pin: string;
+}
+
+export class MakePaymentDto {
+  @IsString() @Length(10, 40)
+  public orderId: string;
+
+  @IsNumber()
+  public readonly totalAmount: number;
+
+  @ValidateNested({ each: true })
+  @Type(() => PaymentInfoDto)
+  public readonly paymentDetails: PaymentInfoDto;
 }
